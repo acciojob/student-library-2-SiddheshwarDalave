2b9e65b96b50ba87b3fdc4c80f92b83b53046bf4
Siddheshwar <siddheshwardalave9119@gmail.com>
2022-12-04 00:21:05 +0530

    commit 11

--- src/main/java/com/driver/models/Author.java
+++ src/main/java/com/driver/models/Author.java
@@ -38,6 +38,14 @@ public class Author {
         this.country=country;
     }
 
+    public Author(int id, String name, String email, int age, String country, List<Book> booksWritten) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.age = age;
+        this.country = country;
+        this.booksWritten = booksWritten;
+    }
 }
 
 
--- src/main/java/com/driver/models/Book.java
+++ src/main/java/com/driver/models/Book.java
@@ -42,5 +42,21 @@ public class Book {
 public Book(){
 
 }
+
+    public Book(String name, Genre genre, Author author) {
+        this.name = name;
+        this.genre = genre;
+        this.author = author;
+    }
+
+    public Book(int id, String name, Genre genre, Author author, Card card, boolean available, List<Transaction> transactions) {
+        this.id = id;
+        this.name = name;
+        this.genre = genre;
+        this.author = author;
+        this.card = card;
+        this.available = available;
+        this.transactions = transactions;
+    }
 }
 
